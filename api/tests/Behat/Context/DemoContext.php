252a96ea7e4874b6e80733ed8c0828570cb57d39
Alg0r3 <15010639+Alg0r3@users.noreply.github.com>
2023-12-24 23:17:37 +0100

    feat(lint): add linting tools to the project
    - add friendsofphp/php-cs-fixer
    - add phpstan/phpstan
    - add a Makefile

--- api/tests/Behat/Context/DemoContext.php
+++ api/tests/Behat/Context/DemoContext.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace App\Tests\Behat\Context;
 
 use Behat\Behat\Context\Context;
-use Exception;
-use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -24,11 +22,12 @@ final class DemoContext implements Context
 
     /**
      * @When a demo scenario sends a request to :path
-     * @throws Exception
+     *
+     * @throws \Exception
      */
     public function aDemoScenarioSendsARequestTo(string $path): void
     {
-        $this->response = $this->kernel->handle(Request::create($path, 'GET'));
+        $this->response = $this->kernel->handle(Request::create($path));
     }
 
     /**
@@ -36,8 +35,8 @@ final class DemoContext implements Context
      */
     public function theResponseShouldBeReceived(): void
     {
-        if ($this->response === null) {
-            throw new RuntimeException('No response received');
+        if (null === $this->response) {
+            throw new \RuntimeException('No response received');
         }
     }
 }
